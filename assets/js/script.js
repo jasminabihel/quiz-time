e0c646b6ec19571dc60e52d500722580b8a5d4d6
Jasmina Bihel <jasmina.bihel@gmail.com>
2023-04-04 20:40:03 +0000

    Add basic structure to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function start(){
+
+}
+function questionArea(){
+
+}
+function questionNumber(){
+
+}
+function chooseAnswer(){
+
+}
+function checkAnswer (){
+
+}
+
+function navigate(){
+
+}
+function score(){
+
+}
