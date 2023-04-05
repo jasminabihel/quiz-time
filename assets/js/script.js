01d4f8697927b5cf951395540181d77770f147b8
Jasmina Bihel <jasmina.bihel@gmail.com>
2023-04-05 21:05:18 +0000

    Add function startGame, nextQuestion, previousQuestion to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {})
 
+let data ={
+    score:0,
+    questionNumber:0,
+};
+
 function startGame() {
-    let startGame = document.addEventListener("click")
-    startGame.document.getElementById("quest ionArea")[0];
+    document.getElementById("questionArea").style.display="block";
+    document.getElementsByClassName("navigate")[0].style.display="block";
+    document.getElementById("textIntro").style.display="none";
+    getNextQuestion();
 }
 
-function questionArea() {
+function getNextQuestion() {
+    let q = questions[data.questionNumber];
+    document.getElementById("question").innerText=q.question;
+    
+    data.questionNumber++;
+    document.getElementById("questionNumber").innerText=data.questionNumber;
+
 
 }
 
-function questionNumber() {
+function getPreviousQuestion() {
+    data.questionNumber--;
+    document.getElementById("questionNumber").innerText=data.questionNumber;
 
 }
-
 function chooseAnswer() {
 
 }
@@ -21,8 +35,25 @@ function checkAnswer() {
 
 }
 
-function navigate() {
-
+function score() {
+    console.log("scoreA")
 }
 
-function score() {}
+let questions = [
+    {
+        question: "en grym fråga",
+        answer1: "asd",
+        answer2: "asd",
+        answer3: "asd",
+        answer4: "asd",
+        correct: 3    
+    },
+    {
+        question: "en annan grym fråga",
+        answer1: "asd",
+        answer2: "asd",
+        answer3: "asd",
+        answer4: "asd",
+        correct: 1    
+    }
+]
